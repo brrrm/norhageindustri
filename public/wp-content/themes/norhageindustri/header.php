9b338ee56c7160b0cc254451713966277356e735
bram de leeuw <bramdeleeuw@gmail.com>
2023-10-30 16:13:50 +0100

    navigation container class

--- public/wp-content/themes/norhageindustri/header.php
+++ public/wp-content/themes/norhageindustri/header.php
@@ -109,15 +109,17 @@
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'norhageindustri' ); ?></button>
 			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
+			wp_nav_menu([
+				'theme_location' 	=> 'menu-1',
+				'menu_id'        	=> 'primary-menu',
+				'container_class'	=> 'menu-main-navigation-container'
+			]);
 			?>
 			<?php
-			wp_nav_menu(['theme_location' => 'menu-2',]);
+			wp_nav_menu([
+				'theme_location' 	=> 'menu-2',
+				'container_class'	=> 'menu-secondary-menu-container'
+			]);
 			?>
 
 			<aside>
