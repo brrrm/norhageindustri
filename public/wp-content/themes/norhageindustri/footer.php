fa1a89b5151e00d1682d2381be1c00e69f53a8d0
Cecile Enzler <cecile.enzler@gmail.com>
2023-11-01 14:28:15 +0100

    Poging tot het logo toevoegen in de footer zodat de code niet onodig groeit.
    Footer aanpassingen zodat het responive word en netter.

--- public/wp-content/themes/norhageindustri/footer.php
+++ public/wp-content/themes/norhageindustri/footer.php
@@ -37,7 +37,7 @@
 				<dd><a href="https://norhage.no" target="_blank">norhage.no</a></dd>
 			</dl>
 		</div>
-
+		<img class="footer-logo" src="<?php echo get_template_directory_uri() . '/images/svg/norhage-logo.svg'; ?>" />
 		<nav id="footer-navigation" class="sitemap">
 			<h2><?php _e('Sitemap', 'norhageindustri'); ?></h2>
 			<button class="menu-toggle" aria-controls="sitemap" aria-expanded="false"><?php esc_html_e( 'Sitemap', 'norhageindustri' ); ?></button>
