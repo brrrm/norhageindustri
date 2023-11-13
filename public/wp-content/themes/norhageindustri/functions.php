3dcd00b5d088b25c2b17d2b3830a55d22bcdc7ce
bram de leeuw <bramdeleeuw@gmail.com>
2023-11-13 15:44:51 +0100

    Testje

--- public/wp-content/themes/norhageindustri/functions.php
+++ public/wp-content/themes/norhageindustri/functions.php
@@ -565,10 +565,6 @@ add_action('init', function () {
  * FIX voor CORS issues met polylang
  */
 add_filter( 'plugins_url', function ( $url ) {
-	if ( false === strpos( $url, 'elementor' ) ) {
-		return $url;
-	}
-
 	$pll_options = get_option( 'polylang' );
 	if ( ! isset( $pll_options['domains'] ) ) {
 	// Not a multidomain configuration.
