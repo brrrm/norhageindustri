36cbf1fd9fb976d057f3e27f8152d2e7d6c99af1
Cecile Enzler <cecile.enzler@gmail.com>
2023-11-15 10:26:42 +0100

    Samen met Bram aangepast

--- public/wp-content/themes/norhageindustri/blocks/cta-within-text-block/ctaWithinTextBlock.php
+++ public/wp-content/themes/norhageindustri/blocks/cta-within-text-block/ctaWithinTextBlock.php
@@ -31,24 +31,26 @@ if ( ! empty( $block['align'] ) ) {
 
 
 <figure <?php echo esc_attr( $anchor ); ?>class="<?php echo esc_attr( $class_name ); ?>" >
-	<div class="image-col">
-		<?php if ( $image ) : ?>
-			<div class="image">
-				<?php echo wp_get_attachment_image( $image['ID'], 'full', '', array( 'class' => 'img', 'alt' => $image['alt'] ) ); ?>
-			</div>
-		<?php else : ?>
-			<div class="dummy">
-			</div>
-		<?php endif; ?>
-		<?php if($phone):?>
-			<p><a href="tel:<?php echo trim(esc_html($phone)); ?>" class="cta-phone button"><?php echo esc_html($phone); ?></a></p>
-		<?php endif; ?>
-		<?php if($email):?>
-			<p><a href="mailto:<?php echo trim(esc_html($email)); ?>" class="cta-email button"><?php echo esc_html($email); ?></a></p>
-		<?php endif; ?>
-	</div>
-	<div class="text-col">
-		<h2><?php echo esc_html( $title ); ?></h2>
-		<div class="text"><?php echo $text; ?></div>
+	<div class="wrapper">
+		<div class="image-col">
+			<?php if ( $image ) : ?>
+				<div class="image">
+					<?php echo wp_get_attachment_image( $image['ID'], 'full', '', array( 'class' => 'img', 'alt' => $image['alt'] ) ); ?>
+				</div>
+			<?php else : ?>
+				<div class="dummy">
+				</div>
+			<?php endif; ?>
+			<?php if($phone):?>
+				<p><a href="tel:<?php echo trim(esc_html($phone)); ?>" class="cta-phone button"><?php echo esc_html($phone); ?></a></p>
+			<?php endif; ?>
+			<?php if($email):?>
+				<p><a href="mailto:<?php echo trim(esc_html($email)); ?>" class="cta-email button"><?php echo esc_html($email); ?></a></p>
+			<?php endif; ?>
+		</div>
+		<div class="text-col">
+			<h2><?php echo esc_html( $title ); ?></h2>
+			<?php echo $text; ?>
+		</div>
 	</div>
 </figure>
