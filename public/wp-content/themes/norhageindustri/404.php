544936854474c0ea648b218a612757ecff239b4a
Cecile Enzler <cecile.enzler@gmail.com>
2023-11-15 12:46:01 +0100

    .button had nu geen styling waar ik tegenaanliep met de 404..
    Ik heb dus de button basisstyling gemaakt met de witte button aangezien dit het meeste gebruikt word op de website.
    Ik heb de 404 pagina aangepast.

--- public/wp-content/themes/norhageindustri/404.php
+++ public/wp-content/themes/norhageindustri/404.php
@@ -17,10 +17,14 @@ get_header();
 				<header class="page-header">
 					<h1 class="page-title"><?php esc_html_e( '404', 'norhageindustri' ); ?></h1>
 				</header><!-- .page-header -->
-				<p><?php esc_html_e( 'oh dear, we hadden een cm te kort, deze pagina is gerecycled' ); ?></p>	
+				<p><?php esc_html_e( 'oh dear, we had to short on material..' ); ?></p>	
+				<a href="<?php echo home_url(); ?>" class="button blue-button" >Back to home</a>
 			</div><!-- .page-content -->
+			<div class="gif">
+				<iframe src="https://giphy.com/embed/1wrRQfA0DcS7n3zaX0" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+				<p><a href="https://giphy.com/gifs/cute-house-beach-1wrRQfA0DcS7n3zaX0">via GIPHY</a></p>
+			</div>
 		</section><!-- .error-404 -->
-
 	</main><!-- #main -->
 
 <?php
