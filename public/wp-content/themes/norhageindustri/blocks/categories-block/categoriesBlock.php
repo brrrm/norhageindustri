124c936fcae9ff961de4153a7029eb7b59a984c4
bram de leeuw <bramdeleeuw@gmail.com>
2023-12-15 16:59:28 +0100

    fallback images in categories block

--- public/wp-content/themes/norhageindustri/blocks/categories-block/categoriesBlock.php
+++ public/wp-content/themes/norhageindustri/blocks/categories-block/categoriesBlock.php
@@ -9,6 +9,21 @@
 $catName		= get_field( 'categories' );
 $terms			= get_terms( ['taxonomy' => $catName, 'hide_empty' => false] );
 
+// set the correct post-type for the fallback query to get the image
+switch($catName){
+	case 'product-type':
+		$post_type = 'product';
+		break;
+	case 'plastic-type':
+		$post_type = 'plastic';
+		break;
+	case 'service-type':
+		$post_type = 'service';
+		break;
+	case 'constr-mat-type':
+		$post_type = 'construction';
+		break;
+}
 
 // Support custom "anchor" values.
 $anchor = '';
@@ -30,12 +45,33 @@ if ( ! empty( $block['align'] ) ) {
 <div <?php echo esc_attr( $anchor ); ?>class="<?php echo esc_attr( $class_name ); ?>" >
 	<ul class="taxonomy-teasers">
 	<?php foreach($terms as $term): ?>
-		<?php $image = get_field('image', $term); ?>
+		<?php 
+			$image = get_field('image', $term);
+			// if no image has been set, let's try to get an image from the first post in this cat
+			if(!$image){
+				$first_post = get_posts([
+					'numberposts'		=> 1,
+					'post_type'			=> $post_type,
+					'tax_query'			=> [
+						[
+							'taxonomy' 			=> $catName,
+							'terms'				=> $term->term_id,
+						]
+					]
+				]);
+
+				if($first_post){
+					$image = get_the_post_thumbnail($first_post[0]->ID, 'medium');
+				}
+			}
+		?>
 		<li class="taxonomy-term teaser image-button">
 			<a href="<?php echo get_term_link( $term ); ?>" title="<?php sprintf( __( 'View all post filed under %s', 'norhageindustri' ), $term->name ); ?>">
 				<?php 
-				if($image){
-					echo wp_get_attachment_image( $image['ID'], 'full', '', array( 'class' => 'header-image__img', 'alt' => $image['alt'] ) );
+				if($image && is_array($image)){
+					echo wp_get_attachment_image( $image['ID'], 'medium', '', array( 'class' => 'header-image__img', 'alt' => $image['alt'] ) );
+				}elseif($image){
+					echo $image;
 				}
 				?>
 			</a>
