ef67b76382a9c6eb1c67176cc20a1d0d6faaed01
bram de leeuw <bramdeleeuw@gmail.com>
2024-01-18 11:25:49 +0100

    fixed navigation

--- public/wp-content/themes/norhageindustri/functions.php
+++ public/wp-content/themes/norhageindustri/functions.php
@@ -438,13 +438,28 @@ add_action( 'init', 'norhageindustri_create_posttypes' );
 function norhage_menu_add_category_posts( $output, $item, $depth, $args ) {
     // Check if the item is a Category or Custom Taxonomy
     if( $args->menu_id == 'primary-menu' && $item->type == 'taxonomy' ) {
-        $object = get_term($item->object_id, $item->object);
+        //$object = get_term($item->object_id, $item->object);
+        switch($item->object){
+        	case 'product-type':
+        		$post_type = 'product';
+        		break;
+        	case 'plastic-type':
+        		$post_type = 'plastic';
+        		break;
+        	case 'constr-mat-type':
+        		$post_type = 'construction';
+        		break;
+        	case 'service-type':
+        		$post_type = 'service';
+        		break;
+        }
+        error_log(print_r($item, true));
         $posts = get_posts([
-        	'post_type'		=> 'product',
+        	'post_type'		=> $post_type,
         	'numberposts'	=> -1,
         	'tax_query'		=> [
         		[
-	        		'taxonomy'		=> 'product-type',
+	        		'taxonomy'		=> $item->object,
 	        		'field'			=> 'term_id',
 	        		'terms'			=> $item->object_id
 	        	]
