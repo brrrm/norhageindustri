92265fc3abbe9938bd210cffd35e4bc33dcfaa9c
bram de leeuw <bramdeleeuw@gmail.com>
2024-04-19 17:07:54 +0200

    consent

--- public/wp-content/themes/norhageindustri/header.php
+++ public/wp-content/themes/norhageindustri/header.php
@@ -28,13 +28,17 @@
 			'analytics_storage': 	'granted'
 		});
 
+	</script>
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-V26DXPHHTF"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		
 		gtag('js', new Date());
-
 		gtag('config', 'G-V26DXPHHTF');
 	</script>
 
-	<!-- Google tag (gtag.js) -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-V26DXPHHTF"></script>
 	
 	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
