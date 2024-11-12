b9b5f63540ac452650b96565ee9ce1b198182b3d
bram de leeuw <bramdeleeuw@gmail.com>
2024-11-12 16:30:44 +0100

    editable white blocks

--- public/wp-content/themes/norhageindustri/blocks/headerblock/headerblock.php
+++ public/wp-content/themes/norhageindustri/blocks/headerblock/headerblock.php
@@ -10,6 +10,7 @@ $title			= get_field( 'title' );
 $text			= get_field( 'intro_text' );
 $image			= get_field( 'image' );
 $usps			= get_field('usps');
+$blocks 		= get_field('white_blocks');
 
 
 // Support custom "anchor" values.
@@ -71,7 +72,9 @@ $url_materials = function_exists('pll_get_post')? esc_url(get_permalink(pll_get_
 						<path class="st0" d="M12.1,27.9L28,12l-6.4-6.4C20,4,17.4,4,15.9,5.6L5.6,15.8c-1.6,1.6-1.6,4.1,0,5.7L12.1,27.9z"/>
 					</g>
 				</svg>
-				<strong><a href="<?php echo $url_greenhouses; ?>"><?php _e('Industrial greenhouse projects', 'norhageindustri'); ?></a></strong>
+				<strong>
+					<a href="<?php echo $blocks[0]['link']['url'] ?? $url_greenhouses; ?>"><?php echo $blocks[0]['link']['title'] ?? __('Industrial greenhouse projects', 'norhageindustri'); ?></a>
+				</strong>
 			</li>
 			<li>
 				<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 60.4" style="enable-background:new 0 0 100 60.4;" xml:space="preserve">
@@ -85,14 +88,18 @@ $url_materials = function_exists('pll_get_post')? esc_url(get_permalink(pll_get_
 					<path class="st0" d="M75.8,0.3C75.5,0,75.2,0,74.8,0S74,0.1,73.7,0.4c-7.4,5.3-13.3,12.2-17.5,20l3.9,1.3C63.8,15,68.7,9.1,74.8,4.4C87.7,14.3,95.6,29.7,96,46H84.7V16c0-1.1-0.9-2-2-2H66.8c-1.1,0-2,0.9-2,2v30h-0.9v4H98c1.1,0,2-0.9,2-2C100.1,29.2,91.2,11.3,75.8,0.3z M80.6,46H68.7V17.9h11.9V46z"/>
 				</svg>
 
-				<strong><a href="<?php echo $url_services; ?>"><?php _e('Installation services', 'norhageindustri'); ?></a></strong>
+				<strong>
+					<a href="<?php echo $blocks[1]['link']['url'] ?? $url_services; ?>"><?php echo $blocks[1]['link']['title'] ?? __('Installation services', 'norhageindustri'); ?></a>
+				</strong>
 			</li>
 			<li>
 				<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve">
 					<style type="text/css">.st0{fill:#0A4164;}</style>
 					<path class="st0" d="M94.5,20L83.7,30.8c-0.2,0.2-0.6,0.3-0.8,0.3l-11.2-2c-0.4-0.1-0.7-0.4-0.8-0.8l-2-11.2c0-0.3,0-0.6,0.3-0.8L80,5.5c0.8-0.8,0.7-2.2-0.2-2.9C77.3,0.8,74.4,0,72.1,0c-0.2,0-0.3,0-0.5,0c-3,0.2-9.4,1.9-14.4,7.1c-4.8,5.1-9.9,14.6-5.4,25.1c0.5,1.3,1.1,3-0.6,4.7c-1.8,1.8-48,45.3-48,45.3c-4.3,3.7-4,10.6,0,14.6c2,2,4.8,3.1,7.5,3.1c2.7,0,5.3-1,7.1-3.1c0,0,43.1-45.9,45.2-48c1-0.9,1.9-1.2,2.7-1.2c0.8,0,1.5,0.3,2.1,0.6c2.4,1.2,5,1.8,7.8,1.8c6.4,0,13.2-3,17.4-7.1c5.9-5.9,6.9-11.5,7.1-14.4c0.1-2.4-0.5-5.4-2.6-8.2C96.7,19.3,95.3,19.2,94.5,20L94.5,20z M13.1,91.7c-1.3,1.3-3.5,1.3-4.8,0c-1.3-1.3-1.3-3.5,0-4.8c1.3-1.3,3.5-1.3,4.8,0C14.4,88.2,14.4,90.4,13.1,91.7z"/>
 				</svg>
-				<strong><a href="<?php echo $url_materials; ?>"><?php _e('Maintenance & restoration', 'norhageindustri'); ?></a></strong>
+				<strong>
+					<a href="<?php echo $blocks[2]['link']['url'] ?? $url_materials; ?>"><?php echo $blocks[2]['link']['title'] ?? __('Maintenance & restoration', 'norhageindustri'); ?></a>
+				</strong>
 			</li>
 			<li>
 				<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve">
@@ -101,7 +108,9 @@ $url_materials = function_exists('pll_get_post')? esc_url(get_permalink(pll_get_
 					<path class="st0" d="M49.2,58.4l-0.4,0.1c-0.8,0.2-1.6,0.4-2.4,0.4H46l-0.5,0c-0.5,0-1-0.1-1.6-0.3l-0.3-0.1l-0.7-0.3c-0.5-0.3-17.5-9-30.4-15.6c-6.1,3.7-10.9,6.7-11.6,7.1c-0.3,0.2-0.6,0.4-0.8,0.8C0,51,0,51.5,0,51.9c0,0,0,0,0,0.1c0,0.2,0.1,0.4,0.2,0.6c0,0,0,0.1,0.1,0.1c0,0,0,0,0,0c0.1,0.1,0.2,0.2,0.3,0.3c0.1,0.1,0.2,0.2,0.4,0.3c0,0,0,0,0,0C1.7,53.7,44.9,76,45.5,76.3c0,0,0.1,0,0.1,0c0.2,0.1,0.4,0.1,0.6,0.2c0.1,0,0.1,0,0.2,0c0.2,0,0.4,0,0.7-0.1c0,0,0.1,0,0.1,0c0.1-0.1,0.3-0.1,0.4-0.2L90,50.2c0.1,0,0.1-0.1,0.2-0.1c0.1-0.1,0.2-0.2,0.3-0.3c0.1-0.1,0.2-0.2,0.3-0.3c0-0.1,0.1-0.1,0.1-0.2c0-0.1,0-0.1,0-0.2c0-0.1,0.1-0.3,0.1-0.4c0-0.1,0-0.3,0-0.4c0-0.1,0-0.3-0.1-0.4c0-0.1-0.1-0.3-0.1-0.4c0-0.1,0-0.1-0.1-0.2c0-0.1-0.1-0.1-0.1-0.2c-0.1-0.1-0.2-0.2-0.3-0.3c-0.1-0.1-0.2-0.2-0.3-0.2c-0.1,0-0.1-0.1-0.2-0.1l-11.3-5.8L50.7,57.8C50.1,58.1,49.6,58.3,49.2,58.4z"/>
 					<path class="st0" d="M45.6,0.2c-0.1,0-0.2,0-0.2-0.1C45.3,0.1,45.1,0.1,45,0c-0.2,0-0.3,0-0.5,0c-0.1,0-0.2,0-0.3,0c-0.2,0-0.4,0.1-0.5,0.2c0,0-0.1,0-0.2,0C43,0.6,3.3,24.9,1.1,26.3c-0.3,0.2-0.6,0.4-0.8,0.8C0,27.5,0,28,0,28.4c0,0,0,0,0,0.1c0,0.2,0.1,0.4,0.2,0.6c0,0,0,0.1,0.1,0.1c0,0,0,0,0,0c0.1,0.1,0.2,0.2,0.3,0.3c0.1,0.1,0.2,0.2,0.4,0.3c0,0,0,0,0,0c0.6,0.3,43.8,22.6,44.4,22.9c0,0,0.1,0,0.1,0c0.2,0.1,0.4,0.1,0.6,0.2c0.1,0,0.1,0,0.2,0c0.2,0,0.4,0,0.7-0.1c0,0,0.1,0,0.1,0c0.1-0.1,0.3-0.1,0.4-0.2L90,26.7c0.1,0,0.1-0.1,0.2-0.1c0.1-0.1,0.2-0.2,0.3-0.3c0.1-0.1,0.2-0.2,0.3-0.3c0-0.1,0.1-0.1,0.1-0.2c0-0.1,0-0.1,0-0.2c0-0.1,0.1-0.3,0.1-0.4c0-0.1,0-0.3,0-0.4c0-0.1,0-0.3-0.1-0.4c0-0.1-0.1-0.3-0.1-0.4c0-0.1,0-0.1-0.1-0.2c0-0.1-0.1-0.1-0.1-0.2c-0.1-0.1-0.2-0.2-0.3-0.3c-0.1-0.1-0.2-0.2-0.3-0.2c-0.1,0-0.1-0.1-0.2-0.1L45.6,0.2z"/>
 				</svg>
-				<strong><a href="<?php echo $url_plastics; ?>"><?php _e('Roofing & facade, PVC curtains', 'norhageindustri'); ?></a></strong>
+				<strong>
+					<a href="<?php echo $blocks[3]['link']['url'] ?? $url_plastics; ?>"><?php echo $blocks[3]['link']['title'] ?? __('Roofing & facade, PVC curtains', 'norhageindustri'); ?></a>
+				</strong>
 			</li>
 		</ul>
 	</div>
