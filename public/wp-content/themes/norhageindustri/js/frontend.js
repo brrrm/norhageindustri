8b822c7764c3e7b42f5d2e65b361661b49b0a7cd
bram de leeuw <bramdeleeuw@gmail.com>
2024-11-12 10:59:26 +0100

    fix springende header

--- public/wp-content/themes/norhageindustri/js/frontend.js
+++ public/wp-content/themes/norhageindustri/js/frontend.js
@@ -9,7 +9,7 @@
 		window.scrollInterval = window.setInterval(function(){
 			if(window.scrollY > window.innerHeight){
 				$('body:not(.small-height-header)').addClass('small-height-header');
-			}else{
+			}else if(window.scrollY < window.innerHeight - $('#masthead').height()){
 				$('body.small-height-header').removeClass('small-height-header');
 			}
 		}, 100);
