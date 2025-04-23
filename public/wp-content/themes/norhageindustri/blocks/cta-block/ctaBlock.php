b8bab3197c656acd5bd9af0bbffecb2474853742
reinikeda <reinikeda@gmail.com>
2025-04-23 08:32:49 +0300

    CTA block update, removed image

--- public/wp-content/themes/norhageindustri/blocks/cta-block/ctaBlock.php
+++ public/wp-content/themes/norhageindustri/blocks/cta-block/ctaBlock.php
@@ -10,7 +10,6 @@ $title			= get_field( 'title' );
 $text			= get_field( 'text' );
 $phone			= get_field( 'phone_number' );
 $email			= get_field( 'email' );
-$image			= get_field( 'image' );
 $contact_form	= get_field( 'contact_form' );
 
 
@@ -33,31 +32,24 @@ if ( empty( $block['align'] ) ) {
 ?>
 
 
-<div <?php echo esc_attr( $anchor ); ?>class="<?php echo esc_attr( $class_name ); ?>" >
-	<div class="image-col">
-		<?php if ( $image ) : ?>
-			<figure class="image">
-				<?php echo wp_get_attachment_image( $image['ID'], 'full', '', array( 'class' => 'img', 'alt' => $image['alt'] ) ); ?>
-			</figure>
-		<?php else : ?>
-			<figure class="dummy">
-			</figure>
-		<?php endif; ?>
-	</div>
-	<div class="text-col">
-		<h2><?php echo esc_html( $title ); ?></h2>
-		<div class="text"><?php echo $text; ?></div>
-		<?php if($phone):?>
-			<p><a href="tel:<?php echo trim(esc_html($phone)); ?>" class="cta-phone button"><?php echo esc_html($phone); ?></a></p>
-		<?php endif; ?>
-		<?php if($email):?>
-			<p><a href="mailto:<?php echo trim(esc_html($email)); ?>" class="cta-email button"><?php echo esc_html($email); ?></a></p>
-		<?php endif; ?>
-		
-		<?php if($contact_form){
-			$hash = get_post_meta($contact_form, '_hash', true);
-			$form_id = substr($hash, 0, 7);
-			echo apply_shortcodes( sprintf('[contact-form-7 id="%s"]', $form_id) );
-		}?>
-	</div>
+<div <?php echo esc_attr( $anchor ); ?>class="<?php echo esc_attr( $class_name ); ?>">
+    <div class="text-col">
+        <h2><?php echo esc_html( $title ); ?></h2>
+        <div class="text"><?php echo $text; ?></div>
+        <?php if ( $phone ) : ?>
+            <p><a href="tel:<?php echo trim( esc_html( $phone ) ); ?>" class="cta-phone button"><?php echo esc_html( $phone ); ?></a></p>
+        <?php endif; ?>
+        <?php if ( $email ) : ?>
+            <p><a href="mailto:<?php echo trim( esc_html( $email ) ); ?>" class="cta-email button"><?php echo esc_html( $email ); ?></a></p>
+        <?php endif; ?>
+    </div>
+    <div class="form-col">
+        <?php
+        if ( $contact_form ) {
+            $hash = get_post_meta( $contact_form, '_hash', true );
+            $form_id = substr( $hash, 0, 7 );
+            echo apply_shortcodes( sprintf( '[contact-form-7 id="%s"]', $form_id ) );
+        }
+        ?>
+    </div>
 </div>
